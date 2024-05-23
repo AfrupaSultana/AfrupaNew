235dad1d8b8fe16b53b5d00a00320e62dac6c434
Afrupa Sultana <168257310+AfrupaSultana@users.noreply.github.com>
2024-05-23 07:55:53 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+app.post('/contact', (req, res) => {
+    //     name = req.body.name
+    //     phonenumber = req.body.phone
+    //     email = req.body.email
+    //     address = req.body.address
+    //     more = req.body.desc
+    //     let outputToWrite = `
+    //      The name of the client is : ${name}
+    //      Age : ${phone} years old 
+    //      Gender : ${email} 
+    //      residing at : ${address}
+    //      More about him/her : ${desc}`
+    //     fs.writeFileSync('output.txt', outputToWrite)
+    //     const params = { 'message': 'Your form has been submitted successfully' };
+    //     res.status(200).render('newForm.pug', params);
+})
